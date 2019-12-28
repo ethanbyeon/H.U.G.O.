11074ed34032b734f78955dd338c91daad3569a7
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2019-12-27 17:16:08 -0800

    Updated ban and softban

--- commands/ban.js
+++ commands/ban.js
@@ -1,28 +1,34 @@
-const Discord = require("discord.js");
-const botconfig = require("../botconfig.json");
-const colors = require("../colors.json");
+const { RichEmbed } = require("discord.js");
+const { red_light } = require("../colors.json");
 
+module.exports = {
 
-module.exports.run = async (bot, message, args) => {
+    config: {
+        name: "ban",
+        description: "Bans a user from the guild!",
+        usage: "!ban",
+        accessableby: "Administrators",
+        aliases: ["b", "banish", "poof", "skidoosh", "adios", "remove"]
+    },
+
+    run: async (bot, message, args) => {
 
     if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You are not authorized to perform this command.");
     if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("Sir, I am not authorized to perform this command.");
 
-    let banMember = message.mentions.members.first() || message.guilds.members.get(args[0]);
-        if(!banMember) return message.channel.send("Please specify a user, sir.");
+    let banMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!banMember) return message.channel.send("Please provide a user to ban!");
 
     let reason = args.slice(1).join(" ");
-        if(!reason) reason = "No reason given.";
+        if(!reason) reason = "No reason given!";
 
-    message.delete();
+    banMember.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`).then(() =>
+    message.guild.ban(banMember, { days: 1, reason: reason})).catch(err => console.log(err));
 
-    banMember.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`).then(() => 
-    message.guild.ban(banMember, {days: 1, reason: reason})).catch(err => console.log(err));
+    message.channel.send(`**${banMember.user.tag}** has been banned`).then(m => m.delete(5000));
 
-    message.channel.send(`**${banMember.user.username}** has been terminated, sir.`);
-
-    let embed = new Discord.RichEmbed()
-        .setColor(colors.red_dark)
+    let embed = new RichEmbed()
+        .setColor(redlight)
         .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
         .addField("Moderation:", "ban")
         .addField("Mutee:", banMember.user.username)
@@ -30,17 +36,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Reason:", reason)
         .addField("Date:", message.createdAt.toLocaleString())
     
-    let sChannel = message.guild.channels.find(c => c.name === "general")
-    sChannel.send(embed);
-
-}
-
-module.exports.config = {
-    
-    name: "ban",
-    description: "Bans a user from the guild.",
-    usage: "!ban <@user> <reason>",
-    accessableby: "Administrator",
-    aliases: ["b", "banish", "poof", "skidoosh", "adios", "remove"]
-
+            let sChannel = message.guild.channels.find(c => c.name === "general");
+            sChannel.send(embed);
+    }
 }
