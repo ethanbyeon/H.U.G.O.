59307256785f1d4d1fba6ab90bb39b9c84ce777f
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2019-12-27 17:04:36 -0800

    Updated unban

--- commands/unban.js
+++ commands/unban.js
@@ -1,30 +1,40 @@
-const Discord = require("discord.js")
-const botconfig = require("../botconfig.json");
-const colors = require("../colors.json");
+const { RichEmbed } = require("discord.js")
+const { redlight } = require("../colors.json");
 
-module.exports.run = async (bot, message, args) => {
+module.exports = {
+    
+    config: {
+        name: "unban",
+        description: "Unbans a user from the guild.",
+        usage: "!unban",
+        accessableby: "Administrators",
+        aliases: ["ub", "unbanish"]
+    },
+    
+    run: async (bot, message, args) => {
 
-    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You are not authorized to perform this command.");
+    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You are not authorized to perform this command.")
 
+	if(isNaN(args[0])) return message.channel.send("You need to provide an ID, sir.");
     let bannedMember = await bot.fetchUser(args[0])
-        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+        if(!bannedMember) return message.channel.send("Please specify a user, sir.");
 
     let reason = args.slice(1).join(" ");
-        if(!reason) reason = "No reason given!";
+        if(!reason) reason = "No reason given.";
 
-    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command!")|
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("Sir, I am not authorized to perform this command.")|
     
     message.delete();
     
     try {
-        message.guild.unban(bannedMember, {reason: reason});
-        message.channel.send(`${bannedMember.tag} has been unbanned from the guild.`);
+        message.guild.unban(bannedMember, reason);
+        message.channel.send(`${bannedMember.tag} has been succesfully unbanned, sir.`);
     } catch(e) {
         console.log(e.message);
     }
 
-    let embed = new Discord.RichEmbed()
-        .setColor(colours.redlight)
+    let embed = new RichEmbed()
+        .setColor(red_ligth)
         .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
         .addField("Moderation:", "unban")
         .addField("Moderated on:", `${bannedMember.username} (${bannedMember.id})`)
@@ -32,17 +42,8 @@ module.exports.run = async (bot, message, args) => {
         .addField("Reason:", reason)
         .addField("Date:", message.createdAt.toLocaleString())
     
-    let sChannel = message.guild.channels.find(c => c.name === "general");
-    sChannel.send(embed);
+            let sChannel = message.guild.channels.find(c => c.name === "general");
+            sChannel.send(embed);
 
-}
-
-module.exports.config = {
-
-    name: "unban",
-    description: "Unbans a user from the guild.",
-    usage: "!unban",
-    accessableby: "Administrators",
-    aliases: ["ub", "unbanish"]
-
+    }
 }
